17c93d8c97a84eaf4744f936230adca0752adf7a
kaluxikS <salamonskip@gmail.com>
2023-03-26 18:08:53 +0200

    Added pieces to board

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -16,9 +16,10 @@ IMAGES = {
 
 # loading all images for pieces
 def loadImages():
-    pieces = ["wp", "wR", "wK", "wB", "wK", "wQ", "bp", "bR", "bK", "bB", "bK", "bQ"]
+    pieces = ["wp", "wR", "wN", "wB", "wK", "wQ", "bp", "bR", "bN", "bB", "bK", "bQ"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
 
 def main():
     p.init()
@@ -37,21 +38,31 @@ def main():
             clock.tick(MAX_FPS)
             p.display.flip()
 
+
 """
 Responsible for all graphics during game
 """
+
+
 def drawGameState(screen, gs):
     drawBoard(screen)
     drawPieces(screen, gs.board)
+
 
 def drawBoard(screen):
     colors = [p.Color("white"), p.Color("grey")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
-            color = colors[((r+c) % 2)]
-            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
-def drawPieces(screen,board):
-    pass
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 if __name__ == "__main__":
